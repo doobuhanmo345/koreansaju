962616acee1d4dfb47acf2c1fb436c31f22a042d
doobuhanmo345 <doobuhanmo3@gmail.com>
2025-12-07 12:24:05 +0900

    Fix: Resolve casing conflict for AppProvider.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { AppProvider } from './context/AppProvider';
+import { AppProvider } from './context/appProvider';
 import { AuthContextProvider } from './context/useAuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
