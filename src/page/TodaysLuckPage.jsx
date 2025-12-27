879827dc924b0143101dc6a47a2e8e00847084b6
doobuhanmo345 <doobuhanmo3@gmail.com>
2025-12-27 16:10:10 +0900

    today year loading

--- src/page/TodaysLuckPage.jsx
+++ src/page/TodaysLuckPage.jsx
@@ -414,7 +414,7 @@ export default function TodaysLuckPage() {
       window.scrollTo({ top: 0, behavior: 'smooth' });
     }
   }, [loading]);
-  return <SajuLoading sajuData={sajuData} />;
+
   return (
     <AnalysisStepContainer
       guideContent={sajuGuide}
